206d4b4e4b51fec61fb31945a20d8311bdff116a
Beatriz <beatriznovais1404@gmail.com>
2025-02-13 14:43:46 -0300

    Cadastro do usuário e login

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Cadastro from './pages/cadastro/Cadastro'
+import { AuthProvider } from './context/AuthContext'
 
 function App() {
   return (
-    <>
+    <AuthProvider>
         <BrowserRouter>
           <Navbar />
           <div className="min-h-[80vh]">
@@ -21,7 +22,7 @@ function App() {
           </div>
           <Footer />
         </BrowserRouter>
-    </>
+    </AuthProvider>
   )
 }
 
